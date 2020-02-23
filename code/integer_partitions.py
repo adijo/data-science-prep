5bb38aac910dc9934317d202f84931469a4a56d3
Aditya Joshi <1adityajoshi@gmail.com>
2020-02-23 14:56:43 +0530

    Add integer partition problem

---
+++ code/integer_partitions.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+def generate_integer_partitions(target: int) -> List[List[int]]:
+    ret_val = list()
+
+    def _generate(n: int, max_val: int, curr_partition: List[int]):
+        if n == 0:
+            ret_val.append(curr_partition.copy()[::-1])
+        else:
+            if max_val <= n:
+                curr_partition.append(max_val)
+                _generate(n - max_val, max_val, curr_partition)
+                curr_partition.pop()
+
+            if max_val > 1:
+                _generate(n, max_val - 1, curr_partition)
+
+    _generate(target, target, list())
+    return ret_val[::-1]
+
+
+assert generate_integer_partitions(4) == [
+    [1, 1, 1, 1],
+    [1, 1, 2],
+    [2, 2],
+    [1, 3],
+    [4]
+]
+
+assert generate_integer_partitions(5) == [
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 2],
+    [1, 2, 2],
+    [1, 1, 3],
+    [2, 3],
+    [1, 4],
+    [5]
+]
