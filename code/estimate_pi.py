0f220605ba1714884429d70b0b3ab957cf05a75f
Aditya Joshi <1adityajoshi@gmail.com>
2020-05-26 18:49:11 -0400

    Minor assertion bug

--- code/estimate_pi.py
+++ code/estimate_pi.py
@@ -15,8 +15,10 @@ def euclidean_distance(a: Point, b: Point) -> float:
     return distance.euclidean((a.x, a.y), (b.x, b.y))
 
 
-def is_inside_circle(a: Point) -> bool:
-    return euclidean_distance(a, Point(0.5, 0.5)) < 0.5
+def is_inside_circle(point: Point) -> bool:
+    circle_center: Point = Point(0.5, 0.5)
+    circle_radius = 0.5
+    return euclidean_distance(point, circle_center) < circle_radius
 
 
 def simulate(n_iter: int) -> float:
@@ -32,4 +34,4 @@ def simulate(n_iter: int) -> float:
 
 
 epsilon = 0.01
-assert (simulate(100000) - math.pi) < epsilon
+assert abs(simulate(100000) - math.pi) < epsilon
