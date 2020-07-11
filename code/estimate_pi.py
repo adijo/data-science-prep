464b266608899cfa75a4888dc5140778889d54c5
Aditya Joshi <1adityajoshi@gmail.com>
2020-07-11 16:04:04 -0400

    Improve API

--- code/estimate_pi.py
+++ code/estimate_pi.py
@@ -1,6 +1,8 @@
 import math
 import random
 from dataclasses import dataclass
+from typing import ClassVar
+
 from common import almost_equal
 
 from scipy.spatial import distance
@@ -12,20 +14,23 @@ class Point:
     y: float
 
 
-def euclidean_distance(a: Point, b: Point) -> float:
-    return distance.euclidean((a.x, a.y), (b.x, b.y))
+class UnitCircle:
+    center: ClassVar[Point] = Point(0.5, 0.5)
+    radius: ClassVar[float] = 0.5
 
+    @staticmethod
+    def euclidean_distance(a: Point, b: Point) -> float:
+        return distance.euclidean((a.x, a.y), (b.x, b.y))
 
-def is_inside_circle(point: Point) -> bool:
-    circle_center = Point(0.5, 0.5)
-    circle_radius = 0.5
-    return euclidean_distance(point, circle_center) < circle_radius
+    def contains(self, point: Point) -> bool:
+        return UnitCircle.euclidean_distance(point, self.center) < self.radius
 
 
 def simulate(n_iter: int) -> float:
+    unit_circle = UnitCircle()
     random_points = (Point(x=random.random(), y=random.random()) for _ in range(n_iter))
     num_points_within_circle: int = sum(
-        [1 if is_inside_circle(point) else 0 for point in random_points]
+        [1 if unit_circle.contains(point) else 0 for point in random_points]
     )
 
     return (float(num_points_within_circle) / n_iter) * 4
