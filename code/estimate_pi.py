64020c5afc7dcc1e80083c425cd6e9b6e44fed18
Aditya Joshi <1adityajoshi@gmail.com>
2020-07-11 15:59:23 -0400

    Minor: use generators & make core more readable

--- code/estimate_pi.py
+++ code/estimate_pi.py
@@ -17,18 +17,15 @@ def euclidean_distance(a: Point, b: Point) -> float:
 
 
 def is_inside_circle(point: Point) -> bool:
-    circle_center: Point = Point(0.5, 0.5)
+    circle_center = Point(0.5, 0.5)
     circle_radius = 0.5
     return euclidean_distance(point, circle_center) < circle_radius
 
 
 def simulate(n_iter: int) -> float:
-
+    random_points = (Point(x=random.random(), y=random.random()) for _ in range(n_iter))
     num_points_within_circle: int = sum(
-        [
-            1 if is_inside_circle(Point(x=random.random(), y=random.random())) else 0
-            for _ in range(n_iter)
-        ]
+        [1 if is_inside_circle(point) else 0 for point in random_points]
     )
 
     return (float(num_points_within_circle) / n_iter) * 4
